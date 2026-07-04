542eb0964c63db796e45f7593d7eee7760272388
Tung Nguyen <55363784+tungnguyen1234@users.noreply.github.com>
2026-07-04 10:05:26 -0500

    Update simple Ising model

--- qrc_circuit.py
+++ qrc_circuit.py
@@ -4,6 +4,11 @@ import pennylane as qml
 # Ignore the DeprecationWarnings coming from qiskit EfficientSU2 that can not be avoided.
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-def qrc_circuit(num_qubits):
+def qrc_circuit(num_qubits, t, num_steps, h, J, edges):
+    dt = t / num_steps # Get number of steps from time t gate
     qml.device("default.qubit", wires=num_qubits)
-    pass
+    for step in range(num_steps):
+        for i in range(num_qubits):
+            qml.RX(2 * h[i] * dt, wires=i)
+        for i, j in edges:
+            qml.IsingZZ(2 * J[i, j] * dt, wires=[i, j])
